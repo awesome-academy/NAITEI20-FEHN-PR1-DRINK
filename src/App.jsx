8a6f534aa2d36f9168823414640b2f4c3e098dbe
thao <nguyen.thi.ngoc.thao@sun-asterisk.com>
2025-02-13 10:42:49 +0700

    Component - Product Card Small

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,50 @@
 import Header from "./components/Header";
 import { Button } from "./components/Button";
-import BlogCard from "./components/BlogCard";
+import Header from "./components/Header";
+
+import Home from "./pages/Home";
+import ProductCardSmall from "./components/product-card/ProductCardSmall";
+import product1 from "./assets/products/product1.jpg";
 
 function App() {
+  const products = [
+    {
+      tag: "Sale",
+      image: product1,
+      name: "RƯỢU VANG ĐÀ LẠT",
+      price: 370000,
+      oldPrice: 450000,
+    },
+    {
+      tag: "Mới",
+      image: product1,
+      name: "RƯỢU VANG ĐÀ LẠT",
+      price: 370000,
+      oldPrice: 450000,
+    },
+    {
+      tag: "Hot",
+      image: product1,
+      name: "RƯỢU VANG ĐÀ LẠT",
+      price: 370000,
+      oldPrice: 450000,
+    },
+  ];
+
   return (
     <>
       <Header />
+      <Home />
       <main className="flex min-h-screen flex-col items-center justify-center space-y-4 bg-white text-center">
         <Button>ADD TO CART</Button>
         <Button size="lg">ĐĂNG NHẬP</Button>
         <BlogCard />
-
-        <div class="triangle"></div>
+        <div className="flex">
+          {products.map((product, index) => (
+            <ProductCardSmall key={index} {...product} />
+          ))}
+        </div>
+        ;<div class="triangle"></div>
       </main>
     </>
   );
