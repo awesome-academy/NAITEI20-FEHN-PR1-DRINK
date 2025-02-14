f5b3e012f0c85646ff556229c7bb1e98b3a526f9
duowngng <duowng.ng@gmail.com>
2025-02-14 08:01:32 +0700

    Page - Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,13 @@
 import Header from "./components/Header";
-import { Button } from "./components/Button";
-import Header from "./components/Header";
-
+import Footer from "./components/Footer";
 import Home from "./pages/Home";
-import ProductCardSmall from "./components/product-card/ProductCardSmall";
-import product1 from "./assets/products/product1.jpg";
 
 function App() {
-  const products = [
-    {
-      tag: "Sale",
-      image: product1,
-      name: "RƯỢU VANG ĐÀ LẠT",
-      price: 370000,
-      oldPrice: 450000,
-    },
-    {
-      tag: "Mới",
-      image: product1,
-      name: "RƯỢU VANG ĐÀ LẠT",
-      price: 370000,
-      oldPrice: 450000,
-    },
-    {
-      tag: "Hot",
-      image: product1,
-      name: "RƯỢU VANG ĐÀ LẠT",
-      price: 370000,
-      oldPrice: 450000,
-    },
-  ];
-
   return (
     <>
-      <Header />
+      <Header isHomePage={true} />
       <Home />
-      <main className="flex min-h-screen flex-col items-center justify-center space-y-4 bg-white text-center">
-        <Button>ADD TO CART</Button>
-        <Button size="lg">ĐĂNG NHẬP</Button>
-        <BlogCard />
-        <div className="flex">
-          {products.map((product, index) => (
-            <ProductCardSmall key={index} {...product} />
-          ))}
-        </div>
-        ;<div class="triangle"></div>
-      </main>
+      <Footer />
     </>
   );
 }
