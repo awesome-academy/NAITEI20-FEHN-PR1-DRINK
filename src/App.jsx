a99721f23e90a50f0292d7424568c20182819e21
duongngt-0538 <nguyen.gia.tung.duong@sun-asterisk.com>
2025-02-18 13:13:05 +0700

    Page - Product detail

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import ProductDetail from "./pages/ProductDetail";
 import Home from "./pages/Home";
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products/:id" element={<ProductDetail />} />
+      </Routes>
       <Footer />
-    </>
+    </Router>
   );
 }
 
