c0eee56d5ca843677c391a6ead95733b0ac6a84f
duongngt-0538 <nguyen.gia.tung.duong@sun-asterisk.com>
2025-02-19 13:15:11 +0700

    Page - About + 404

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,84 @@ import ProductDetail from "./pages/ProductDetail";
 import Orders from "./pages/Orders";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import NotFound from "./pages/404";
 import Home from "./pages/Home";
 import PopUp from "./components/PopUp";
 
 function App() {
   return (
     <Router>
-      <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products/:id" element={<ProductDetail />} />
-        <Route path="/orders" element={<Orders />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
+        <Route
+          path="*"
+          element={
+            <>
+              <NotFound />
+            </>
+          }
+        />
+        <Route
+          path="/"
+          element={
+            <>
+              <Header />
+              <Home />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/products/:id"
+          element={
+            <>
+              <Header />
+              <ProductDetail />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/orders"
+          element={
+            <>
+              <Header />
+              <Orders />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/sign-in"
+          element={
+            <>
+              <Header />
+              <SignIn />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/sign-up"
+          element={
+            <>
+              <Header />
+              <SignUp />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/about"
+          element={
+            <>
+              <Header />
+              <About />
+              <Footer />
+            </>
+          }
+        />
       </Routes>
-      <Footer />
     </Router>
   );
 }
